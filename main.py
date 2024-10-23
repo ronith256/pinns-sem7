28daf16466cbecce031b2910a2f63b87d02491c2
Ronith <ronithtg@gmail.com>
2024-10-23 22:55:30 +0530

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vanilla import FlowPINN, train_model
 from flow_visualizer import FlowVisualizer
+from matplotlib.pyplot import plot
 
 # Initialize and train
 domain_params = {'L': 25 * 0.00116, 'H': 0.5 * 0.00116}
@@ -8,4 +9,5 @@ trained_model = train_model(model, num_epochs=100)
 
 # Visualize
 visualizer = FlowVisualizer(domain_params)
-visualizer.plot_flow_field(trained_model, t=15.0, title="Flow Field")
+fig = visualizer.plot_flow_field(trained_model, t=15.0, title="Flow Field")
+plot(fig)
